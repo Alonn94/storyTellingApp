9b01743ed94646c13ff8797cd1cc3465419e998d
Alonn Barthels <alonnbarthels@Alonns-MacBook-Air-2.local>
2025-05-02 09:44:10 +0300

    Add GET /api/stories route

--- backend/routes/storyRoutes.ts
+++ backend/routes/storyRoutes.ts
@@ -3,13 +3,13 @@
 import express from "express";
 import { authenticateToken } from "../middleware/authMiddleware";
 import { authorizeAuthor } from "../middleware/authMiddleware";
-import { deleteStory } from "../controllers/storyController";
-import { postStory } from "../controllers/storyController";
+import { postStory,getAllStories, deleteStory } from "../controllers/storyController";
 import asyncHandler from "../utils/asyncHandler";
 
 const router = express.Router();
 
 router.delete("/:storyId", authenticateToken, authorizeAuthor, deleteStory);
 router.post("/", authenticateToken, asyncHandler(postStory));
+router.get("/", authenticateToken, asyncHandler(getAllStories));
 
 export default router;
