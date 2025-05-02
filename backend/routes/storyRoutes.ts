8dc348ee7518799d9e7cc35891d8e2c03e9fb7f4
Alonn Barthels <alonnbarthels@Alonns-MacBook-Air-2.local>
2025-05-02 09:23:43 +0300

    fix typo

--- backend/routes/storyRoutes.ts
+++ backend/routes/storyRoutes.ts
@@ -3,8 +3,8 @@
 import express from "express";
 import { authenticateToken } from "../middleware/authMiddleware";
 import { authorizeAuthor } from "../middleware/authMiddleware";
-import { deleteStory } from "../controllers/storyContoller";
-import { postStory } from "../controllers/storyContoller";
+import { deleteStory } from "../controllers/storyController";
+import { postStory } from "../controllers/storyController";
 import asyncHandler from "../utils/asyncHandler";
 
 const router = express.Router();
